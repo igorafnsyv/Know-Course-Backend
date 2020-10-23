8bcf3e5629e2f80af668411b3e60cee1c9901f5e
Igor Afanasyev <igor.afanasyev.msk@gmail.com>
2020-10-23 12:24:53 +0800

    Make course code PK of Course model to support path course/COURSE_CODE

--- coursereview/models.py
+++ coursereview/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Course(models.Model):
-    code = models.CharField(max_length=8)
+    code = models.CharField(max_length=8, primary_key=True, unique=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     # course may have many pre-requisites.  Course may be pre-requisite to many courses
