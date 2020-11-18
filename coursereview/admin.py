903c4bdc5f7ad5ee6b333c38dce92bf93acc6a78
Igor Afanasyev <igor.afanasyev.msk@gmail.com>
2020-11-19 00:05:34 +0800

    Register models in admin site

--- coursereview/admin.py
+++ coursereview/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from coursereview.models import *
+
+admin.site.register(UserProfile)
+admin.site.register(Course)
+admin.site.register(Review)
+
 # Register your models here.
